87241ef73a16d49f1c6c16167bf89c2789fc5de6
manassehl9 <manassehl9@gmail.com>
2020-02-11 21:58:34 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/jbpm_json_to_form/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jbpm_json_to_form
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
