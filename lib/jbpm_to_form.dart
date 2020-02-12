d8fad3e2a2ccf51555bd18b51cfcba118ecc2c2a
manassehl9 <manassehl9@gmail.com>
2020-02-12 13:22:33 +0100

    transform json to TextBox, TextArea and IntegerBox

--- lib/jbpm_to_form.dart
+++ lib/jbpm_to_form.dart
@@ -7,6 +7,8 @@ import 'package:flutter/material.dart';
 class JbpmForm extends StatefulWidget {
   final String form;
   final Map formMap;
+  final double padding;
+  final Map errorMessages;
   final Widget buttonSave;
   final ValueChanged<dynamic> onChanged;
 
@@ -14,6 +16,8 @@ class JbpmForm extends StatefulWidget {
     @required this.form,
     @required this.onChanged,
     this.formMap,
+    this.padding,
+    this.errorMessages = const {},
     this.buttonSave,
   });
 
@@ -24,9 +28,95 @@ class JbpmForm extends StatefulWidget {
 class _JbpmFormState extends State<JbpmForm> {
   final dynamic formGeneral;
 
+  String isRequired(item, value) {
+    if (value.isEmpty) {
+      return widget.errorMessages[item['name']] ?? 'Please enter some text';
+    }
+    return null;
+  }
+
+  String validateEmail(item, String value) {
+    String p = "[a-zA-Z0-9\+\.\_\%\-\+]{1,256}" +
+        "\\@" +
+        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+        "(" +
+        "\\." +
+        "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+        ")+";
+    RegExp regExp = new RegExp(p);
+
+    if (regExp.hasMatch(value)) {
+      return null;
+    }
+    return 'Email is not valid';
+  }
+
+  List<Widget> jbpmToForm() {
+    List<Widget> listWidget = List<Widget>();
+    for (var i = 0; i < formGeneral['fields'].length; i++) {
+      Map item = formGeneral['fields'][i];
+
+      if (item['code'] == 'TextBox' ||
+          item['code'] == 'TextArea' ||
+          item['code'] == 'IntergerBox') {
+        listWidget.add(
+          Container(
+            margin: EdgeInsets.only(top: 5.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  onSaved: null,
+                  controller: null,
+                  keyboardType: item['code'] == 'IntergerBox'
+                      ? TextInputType.number
+                      : TextInputType.text,
+                  initialValue: formGeneral['fields'][i]['value'] ?? null,
+                  maxLength: item['maxLength'] ?? null,
+                  maxLines: item['code'] == 'TextArea' ? 10 : 1,
+                  readOnly: item['readOnly'] ?? false,
+                  obscureText: item['code'] == 'Password' ? true : false,
+                  validator: (value) {
+                    if (item['code'] == 'Email') {
+                      return validateEmail(item, value);
+                    }
+
+                    if (item.containsKey('required')) {
+                      if (item['required'] == true ||
+                          item['required'] == 'True' ||
+                          item['required'] == 'true') {
+                        return isRequired(item, value);
+                      }
+                    }
+                    return null;
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    }
+    return listWidget;
+  }
+
   _JbpmFormState(this.formGeneral);
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    print(formGeneral);
+    return Form(
+      autovalidate: formGeneral['autoValidated'] ?? false,
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(widget.padding ?? 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: jbpmToForm(),
+        ),
+      ),
+    );
   }
 }
--- test/jbpm_to_form_test.dart
+++ test/jbpm_to_form_test.dart
@@ -3,5 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import '../lib/jbpm_to_form.dart';
 
 void main() {
-  test('set up the json form', () {});
+  test('set up the json form', () {
+    final jbpmToForm = JbpmForm(
+      form: null,
+      onChanged: null,
+    );
+  });
 }
