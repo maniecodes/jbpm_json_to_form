420fbd332a885805fffb94c6ff6c2a6a00eedd48
manassehl9 <manassehl9@gmail.com>
2020-02-14 14:44:30 +0100

    implement file picker

--- lib/jbpm_to_form.dart
+++ lib/jbpm_to_form.dart
@@ -203,8 +203,66 @@ class _JbpmFormState extends State<JbpmForm> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               RaisedButton(
-                onPressed: () => _openFileExplorer(),
+                onPressed: () async {
+                  setState(() => _loadingPath = true);
+                  try {
+                    _path = null;
+                    _paths = await FilePicker.getMultiFilePath(
+                        type: _pickingType, fileExtension: _extension);
+                    formGeneral['fields'][i]['value'] = _paths;
+                    _handleChanged();
+                  } on PlatformException catch (e) {
+                    print("Unsupported operation" + e.toString());
+                  }
+                  if (!mounted) return;
+                  setState(() {
+                    _loadingPath = false;
+                    _fileName = _path != null
+                        ? _path.split('/').last
+                        : _paths != null ? _paths.keys.toString() : '...';
+                  });
+                  //  }
+                },
                 child: Text('Open file'),
+              ),
+              Builder(
+                builder: (BuildContext context) => _loadingPath
+                    ? Padding(
+                        padding: const EdgeInsets.only(bottom: 10.0),
+                        child: const CircularProgressIndicator())
+                    : _path != null || _paths != null
+                        ? new Container(
+                            padding: const EdgeInsets.only(bottom: 30.0),
+                            height: MediaQuery.of(context).size.height * 0.50,
+                            child: new Scrollbar(
+                                child: new ListView.separated(
+                              itemCount: _paths != null && _paths.isNotEmpty
+                                  ? _paths.length
+                                  : 1,
+                              itemBuilder: (BuildContext context, int index) {
+                                final bool isMultiPath =
+                                    _paths != null && _paths.isNotEmpty;
+                                final String name = 'File $index: ' +
+                                    (isMultiPath
+                                        ? _paths.keys.toList()[index]
+                                        : _fileName ?? '...');
+                                final path = isMultiPath
+                                    ? _paths.values.toList()[index].toString()
+                                    : _path;
+
+                                return new ListTile(
+                                  title: new Text(
+                                    name,
+                                  ),
+                                  subtitle: new Text(path),
+                                );
+                              },
+                              separatorBuilder:
+                                  (BuildContext context, int index) =>
+                                      new Divider(),
+                            )),
+                          )
+                        : new Container(),
               ),
             ],
           ),
